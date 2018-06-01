5c01a1d7352571f600e54a5d67e046a9fc1dff01
David Hu <david.hu@cootek.cn>
2018-06-01 22:59:11 +0800

    [feature] add basic UI VC

---
+++ Suan/Config/SuanConfigViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SuanConfigViewController.swift
+//  Suan
+//
+//  Created by David Hu on 2018/6/1.
+//  Copyright © 2018 DavidHu. All rights reserved.
+//
+
+import UIKit
+
+class SuanConfigViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
